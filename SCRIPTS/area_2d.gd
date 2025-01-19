da07d7a6a0dc6dac39622c76dc09ed651ad23cfc
Matheus Farina <farinamatheus07@gmail.com>
2025-01-18 23:05:30 -0300

    codigo da vaca arrumado

--- SCRIPTS/area_2d.gd
+++ SCRIPTS/area_2d.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+# YOL's aiming function -> Using lambda to always aim the closest mobs
+func _physics_process(delta):
+	var enemies_in_range = get_overlapping_bodies().map(position_from_array)
+	if enemies_in_range.size() > 0:
+		var target_enemy = enemies_in_range.front()
+		for enemy in enemies_in_range:
+			if enemy.distance_to(global_position) < target_enemy.distance_to(global_position):
+				target_enemy = enemy
+		look_at(target_enemy)
+		
+		
+func position_from_array(enemy):
+	return enemy.global_position
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#Mardo arrumou o codigo da vaca
